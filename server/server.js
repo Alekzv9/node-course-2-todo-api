175c1375b058e2f226b94c5b518b1bf7b7f097e4
alekzgrinder@gmail.com <alekzgrinder@gmail.com>
2018-04-03 21:16:01 -0500

    Add GET todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos', (req, res, err) => {
     });
 });
 
+app.get('/todos', (req, res, err) => {
+    Todo.find()
+        .then(todos => {
+            res.send({ todos });
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+});
+
 app.listen(3000, () => {
     console.log('Started on PORT 3000');
 });
