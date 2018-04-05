39bcb11eaa1874628c85972d9b576ae0f6831a42
alekzgrinder@gmail.com <alekzgrinder@gmail.com>
2018-04-05 08:54:53 -0500

    Update config to user json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,24 @@
 //Environment variables
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    //if npm test, package.json changes the env to test
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json');
+    const envConfig = config[env];
+    // process.env.PORT = config[env].PORT;
+    // process.env.MONGODB_URI = config[env].MONGODB_URI;
+
+    //Use forEach to do it dinamically
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
-//else is 'production' filled by heroku
+
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     //if npm test, package.json changes the env to test
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
+//else is 'production' filled by herok
